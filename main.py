206638f6da998bc775c461972508938989f5c981
Aggrey Osango <hurrycane661@gmail.com>
2023-09-07 21:38:41 +0300

    everything is working except fot the corner radius of the label, this needs fixing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import customtkinter as ctk
 # import tkinter as tk
+from tkinter import *
 
 ctk.set_appearance_mode ("light")
 ctk.set_default_color_theme ("blue")
@@ -19,19 +20,23 @@ tbox = ctk.CTkTextbox (master = frame, width = 400, height = 200,
 
 tbox.grid(row = 0, column = 0, sticky ="nsew")
 
+str_var = StringVar ()
+
 def save ():
-    print(tbox.get(index1 = "0.0", index2 = "end"))
+    tb = tbox.get(index1 = "0.0", index2 = "end")
+    str_var.set(str(tb))
 
 
 # Button Command
-btn_01 = ctk.CTkButton(master = frame, text = "Save", command = save)
+btn_01 = ctk.CTkButton(master = frame, text = "Save",
+                       corner_radius = 10, command = save)
 btn_01.grid(padx=5, pady=5)
 
 # Adding a label obj to the frame
-lbl_01 = ctk.CTkLabel (master = frame, width = 50,text = "Name",
-                       bg_color = ("white", "grey75"), corner_radius = 10)
+lbl_01 = ctk.CTkLabel (master = frame, width = 150,textvariable = str_var,
+                       bg_color = "white")
 
-lbl_01.grid(padx=5, pady=5)
+lbl_01.grid(padx=10, pady=5)
 
 if __name__ == '__main__':
     app.mainloop ()
