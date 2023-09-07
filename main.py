12bda684db18b610b785e9cbb5a58a46830d85a6
Aggrey Osango <hurrycane661@gmail.com>
2023-09-07 20:21:49 +0300

    this version of the app has got a scroll bar, text box and a button

---
+++ main.py
@@ -0,0 +1,31 @@
+import customtkinter as ctk
+# import tkinter as tk
+
+ctk.set_appearance_mode ("light")
+ctk.set_default_color_theme ("blue")
+
+# App initialization
+app = ctk.CTk()
+app.geometry("600x520")
+app.title ("Helium Notes")
+
+# Frame Initialization
+frame = ctk.CTkFrame (master = app, width = 590, height = 510, corner_radius = 5)
+frame.pack(padx=10, pady=10)
+
+# TextBox Initialization
+tbox = ctk.CTkTextbox (master = frame, width = 400, height = 200,
+                       scrollbar_button_color = "purple")
+
+tbox.grid(row = 0, column = 0, sticky ="nsew")
+
+def save ():
+    print(tbox.get(index1 = "0.0", index2 = "end"))
+
+
+# Button Command
+btn_01 = ctk.CTkButton(master = frame, text = "Save", command = save)
+btn_01.grid(padx=5, pady=5)
+
+if __name__ == '__main__':
+    app.mainloop ()
